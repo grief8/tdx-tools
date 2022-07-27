d4790cac91cf4933c753e71d68d7e5a5e29d79e8
Fabing Li <lifabing1349212501@gmail.com>
2022-07-27 16:58:51 +0800

    add test module

--- tests/tests/test_tdvm_launch.py
+++ tests/tests/test_tdvm_launch.py
@@ -7,7 +7,7 @@ import logging
 import pytest
 
 from pycloudstack.vmparam import VM_STATE_SHUTDOWN, VM_STATE_RUNNING, \
-    VM_STATE_PAUSE, VM_TYPE_TD, VM_TYPE_LEGACY, VMSpec
+    VM_STATE_PAUSE, VM_TYPE_TD, VM_TYPE_EFI, VM_TYPE_LEGACY, VMSpec
 
 
 __author__ = 'fabing'
@@ -34,31 +34,42 @@ def test_tdvm_lifecycle_virsh(vm_factory):
     Step 6: Start TD guest and check whether status is running
     """
 
-    LOG.info("Create TD guest")
-    inst = vm_factory.new_vm(VM_TYPE_TD, auto_start=True, vmspec=VMSpec.model_base())
-    inst.wait_for_ssh_ready()
+    cpus = [i for i in range(4, 64, 4)]
+    memsize = [i for i in range(4, 128, 8)]
+    for cores in cpus:
+        for mem in memsize:
+            for _ in range(20):
+                LOG.info("VM with {} CPU and {} GB vRAM".format(cores, mem))
+                vm = VMSpec(sockets=1, cores = cores, threads=1, memsize=mem*1024*1024)
 
-    LOG.info("Suspend TD guest")
-    inst.suspend()
-    ret = inst.wait_for_state(VM_STATE_PAUSE)
-    assert ret, "Suspend timeout"
+                LOG.info("Create TD guest")
+                inst = vm_factory.new_vm(VM_TYPE_TD, auto_start=False, vmspec=vm)
+                inst.wait_for_ssh_ready()
 
-    LOG.info("Resume TD guest")
-    inst.resume()
-    ret = inst.wait_for_state(VM_STATE_RUNNING)
-    assert ret, "Resume timeout"
+                LOG.info("Suspend TD guest")
+                inst.suspend()
+                ret = inst.wait_for_state(VM_STATE_PAUSE)
+                assert ret, "Suspend timeout"
 
-    LOG.info("Reboot TD guest")
-    inst.reboot()
-    ret = inst.wait_for_state(VM_STATE_RUNNING)
-    assert ret, "Reboot timeout"
+                LOG.info("Resume TD guest")
+                inst.resume()
+                ret = inst.wait_for_state(VM_STATE_RUNNING)
+                assert ret, "Resume timeout"
 
-    LOG.info("Shutdown TD guest")
-    inst.shutdown()
-    ret = inst.wait_for_state(VM_STATE_SHUTDOWN)
-    assert ret, "Fail to shutdown instance"
+                LOG.info("Reboot TD guest")
+                inst.reboot()
+                ret = inst.wait_for_state(VM_STATE_RUNNING)
+                assert ret, "Reboot timeout"
 
-    LOG.info("Start TD guest")
-    inst.start()
-    ret = inst.wait_for_state(VM_STATE_RUNNING)
-    assert ret, "Fail to start instance"
+                LOG.info("Shutdown TD guest")
+                inst.shutdown(mode="acpi")
+                ret = inst.wait_for_state(VM_STATE_SHUTDOWN)
+                assert ret, "Fail to shutdown instance"
+
+                LOG.info("Start TD guest")
+                inst.start()
+                ret = inst.wait_for_state(VM_STATE_RUNNING)
+                assert ret, "Fail to start instance"
+
+                LOG.info("Destroy TD guest")
+                inst.destroy(delete_image=True, delete_log=True)
